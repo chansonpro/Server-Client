92a236247b1116210960842be5e15bd2cb13f112
chansonpro <1527540881@qq.com>
2017-12-29 20:11:10 +0800

    单例模式中，懒汉模式的升级版本

--- src/main/java/javaDemo/SingleTon.java
+++ src/main/java/javaDemo/SingleTon.java
@@ -19,11 +19,28 @@ public class SingleTon {
 class SingleTon2{
     private SingleTon2() {
     }
-    private static SingleTon2 singleTon2 = new SingleTon2();
+    private static SingleTon2 singleTon2 = null;
     private static synchronized SingleTon2 getInstance(){
         if (singleTon2 == null){
             singleTon2 = new SingleTon2();
         }
         return singleTon2;
+    }
+}
+// 懒汉模式升级版
+class SingleTon3{
+    private static SingleTon3 singleTon3 = null;
+
+    private SingleTon3() {
+    }
+    private static SingleTon3 getSingleTon3(){
+        if (singleTon3 == null){
+            synchronized (SingleTon3.class){
+                if (singleTon3 == null ){
+                    singleTon3 = new SingleTon3();
+                }
+            }
+        }// end if
+        return singleTon3;
     }
 }
